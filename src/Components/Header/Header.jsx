f59e137f66ca6e922bb11cdc5da5aa7684ce7bac
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-09-27 22:31:48 +0530

    *

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,23 +8,25 @@ const Header = () => {
       <div className="nav_link">
         <div className="logo">E Waste</div>
         <div className="link">
-          <Link to={"/"} style={{ textDecoration: "none" }}>
+          <Link to={"/"} style={{ textDecoration: "none" }} className="link_a">
             <p>Home</p>
           </Link>
-          <Link to={"/nearby"} style={{ textDecoration: "none" }}>
+          <Link to={"/nearby"} style={{ textDecoration: "none" }} className="link_a" >
             <p>Nearby</p>
           </Link>
-          <Link to={"/message"} style={{ textDecoration: "none" }}>
+          <Link to={"https://chat-app-frontend-lnr2un0ok-sinha-niranjan.vercel.app/app"} style={{ textDecoration: "none" }} className="link_a">
             <p>Message</p>
           </Link>
-          <Link to={"/contact"} style={{ textDecoration: "none" }}>
+          <Link to={"/contact"} style={{ textDecoration: "none" }} className="link_a">
             <p>Contact</p>
           </Link>
-          <Link to={"/about"} style={{ textDecoration: "none" }}>
+          <Link to={"/about"} style={{ textDecoration: "none" }} className="link_a">
             <p>About</p>
           </Link>
-          <Link to={"/user_login"} style={{ textDecoration: "none" }}>
-            <p className="login_btn">Login</p>
+          
+          <Link to={"/user_login"} style={{ textDecoration: "none" }} className="login_btn">
+
+            <h5>Login</h5>
           </Link>
         </div>
       </div>
